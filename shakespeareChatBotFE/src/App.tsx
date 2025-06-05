424cd681e2e5c06e56de6467d9aad0b31f416ea7
Hasan Miraz <hasibul.miraz@gmail.com>
2025-06-05 23:25:29 +1000

    title bar and logo update FE

--- shakespeareChatBotFE/src/App.tsx
+++ shakespeareChatBotFE/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ChatPage from './ChatPage';
+import ChatPage from "./ChatPage";
 
 const App: React.FC = () => {
   return (
-    <div className="h-screen bg-gray-900 flex items-center justify-center">
-      <div className="w-1/2 h-full">
-        <ChatPage />
+    <div className="App">
+      <div className="h-screen bg-gray-900 flex items-center justify-center">
+        <div className="w-1/2 h-full">
+          <ChatPage />
+        </div>
       </div>
     </div>
   );
